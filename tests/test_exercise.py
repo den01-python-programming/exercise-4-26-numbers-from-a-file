6edc4f42b206e0ca09d096238d0ff9e7a7c8ceee
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:56:06 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,5 +2,26 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    input_values = ["numbers-1.txt","5","20","numbers-1.txt","0","300"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["File?","Lower bound?","Upper bound?","Numbers: 2",\
+                      "File?","Lower bound?","Upper bound?","Numbers: 4"]
